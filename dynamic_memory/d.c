ef6d8f1cda999f93aa80daef636c9d72fb7463b9
Mrudula Thange <thangemrudula@gmail.com>
2025-10-18 17:55:04 +0530

    Add files via upload

---
+++ dynamic_memory/d.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int size;
+    printf("Enter size to be allocated dynamically: ");
+    scanf("%d", &size);
+
+    int *address = (int *)malloc(size * sizeof(int));
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("Enter element at position %d: ", i);
+        scanf("%d", address + i);
+    }
+
+    // DISPLAY
+    printf("Elements in dynamic arrays are: \n");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d\t", *(address + i));
+    }
+
+    free(address);
+}
+/*
+1. malloc
+2. calloc
+3. realloc
+4. free
+*/
